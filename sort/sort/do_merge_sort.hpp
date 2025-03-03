d4febeeb56f3426850c50278da4ba1f81313da88
Ilya Bublik <ilabublik6@gmail.com>
2025-03-03 22:41:52 +0300

    obychaika + sort

--- sort/sort/do_merge_sort.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-namespace mia{
-    void do_merge(int * const arr, const int l, const int q, const int r);
-    void do_merge_sort(int * const arr, const int n);
-    void merge_sort(int * const arr, const int l, const int r);
-}
